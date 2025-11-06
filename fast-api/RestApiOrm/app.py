68728d7e80ad9665b7961a29502d7694e71783bd
Mukund-Kumar7255 <kumarmukund632@gmail.com>
2025-11-06 14:54:10 +0530

    intial commit

--- fast-api/RestApiOrm/app.py
+++ fast-api/RestApiOrm/app.py
@@ -12,5 +12,20 @@ DB_HOST=os.getenv("db_host")
 DB_PORT=os.getenv("db_port")
 DB_NAME=os.getenv("db_name")
 app = FastAPI(title="CRUD APPLICATION")
+# print(DB_NAME)
 
+DATABASE_URI=F"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine=create_engine(DATABASE_URI)
 
+@app.get("/")
+def root():
+    return {"message": "Welcome to the CRUD Application"}
+
+@app.get("/test-db")
+def test_db():
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT NOW()"))
+            return{"status":f"connected{result.fetchall()}"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
